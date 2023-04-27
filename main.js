0be672129135ba9e1c35469877e711d60dc0b470
zhaochuantao <2250386552@qq.com>
2023-04-27 10:17:25 +0800

    home开发完成

--- main.js
+++ main.js
@@ -2,12 +2,32 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
-
+import { $http } from '@escook/request-miniprogram'
 Vue.config.productionTip = false
+uni.$http = $http
+$http.baseUrl = 'https://www.uinav.com'
 
+//请求拦截器
+$http.beforeRequest = function (options){
+	uni.showLoading({
+		title: '数据加载中'
+	})
+}
+//响应拦截器
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+//封装
+uni.$showMsg  = function (title = '数据加载失败',duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+}
 App.mpType = 'app'
 
-const app = new Vue({
+const app = new Vue({ 
     ...App
 })
 app.$mount()
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,38 @@
 <template>
 	<view>
-		
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item v-for="(item,i) in swiperList" :key="i">
+				<navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id">
+					<image :src="item.image_src" alt="">
+				</navigator>
+			</swiper-item>
+		</swiper>
+		<view class="nav-list">
+		   <view class="nav-item" v-for="(item, i) in navList" :key="i" @click="navClickHandler(item)">
+			<image :src="item.image_src" class="nav-img"></image>
+		   </view>
+		</view>
+		<view class="floor-list">
+			<view class="floor-item" v-for="(item,i) in floorList" :key="i">
+				<image :src="item.floor_title.image_src" class="floor-title"></image>
+				<!-- 楼层图片区域 -->
+				<view class="floor-img-box">
+				  <!-- 左侧大图片的盒子 -->
+				  <navigation class="left-img-box" :url="item.product_list[0].url">
+				    <image :src="item.product_list[0].image_src" :style="{width: item.product_list[0].image_width + 'rpx'}" mode="widthFix"></image>
+				  </navigation>
+				  <!-- 右侧 4 个小图片的盒子 -->
+				  <view class="right-img-box">
+				    <navigator class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0" :url="item2.url">
+				      <image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}"></image>
+				    </navigator>
+				  </view>
+			</view>
+		</view>
+	
+		</view>
+
+</view>
 	</view>
 </template>
 
@@ -8,12 +40,90 @@
 	export default {
 		data() {
 			return {
-				
+				swiperList: [],
+				navList: [],
+				floorList: [],
 			};
+		},
+		onLoad() {
+			this.getSwiperList()
+			this.getNavList()
+			this.getFloorList()
+		},
+		methods: {
+			//发起请求
+			async getSwiperList() {
+				const {data:res} = await uni.$http.get('/api/public/v1/home/swiperdata')
+			//请求失败
+			if(res.meta.status !== 200){
+				return uni.$showMsg()
+			}
+			//如果请求成功
+			this.swiperList = res.message
+		    },
+		    async getNavList() {
+			const {data: res } = await uni.$http.get('/api/public/v1/home/catitems')
+			if (res.meta.status !== 200) return uni.$showMsg()
+			this.navList = res.message
+			
+		     },
+		     async getFloorList(){
+		     	const {data:res} = await uni.$http.get('/api/public/v1/home/floordata')
+				if (res.meta.status !== 200){
+					return uni.$showMsg()
+				}
+				 res.message.forEach(floor => {
+				    floor.product_list.forEach(prod => {
+				      prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
+				    })
+				  })
+				this.floorList = res.message
+		     },
+		navClickHandler(item) {
+			if(item.name === '分类') {
+				uni.switchTab({
+					url:'/pages/cate/cate'
+				})
+			}
+		}
 		}
 	}
 </script>
 
 <style lang="scss">
+swiper {
+ height: 330rpx;
 
+ .swiper-item,
+ image {
+   width: 100%;
+   height: 100%;
+ }
+ 
+}
+.nav-list {
+   display: flex;
+   justify-content: space-around;
+   margin: 15px 0;
+ 
+   .nav-img {
+     width: 128rpx;
+     height: 140rpx;
+   }
+ }
+ .floor-title {
+   height: 60rpx;
+   width: 100%;
+   display: flex;
+ }
+ .right-img-box {
+   display: flex;
+   flex-wrap: wrap;
+   justify-content: space-around;
+ }
+ 
+ .floor-img-box {
+   display: flex;
+   padding-left: 10rpx;
+ }
 </style>
